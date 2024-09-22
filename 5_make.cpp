74f304f9e1a4725c831def9249d5120667628162
Trevor Swarm <tswarm@lccc.wy.edu>
2024-09-22 16:10:02 +0000

    Intial documentaion and stub

---
+++ 5_make.cpp
@@ -0,0 +1,19 @@
+
+
+/*
+
+Write a program whose inputs are three integers, and whose output is the smallest of the three values.
+
+Ex: If the input is:
+
+7 15 3
+the output is:
+
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
