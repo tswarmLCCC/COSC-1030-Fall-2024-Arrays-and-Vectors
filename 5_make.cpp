7889ab3b74c9063ffe738f0e667853e0f0a4d792
Trevor Swarm <tswarm@lccc.wy.edu>
2024-10-03 14:51:17 -0600

    Update 5_make.cpp

--- 5_make.cpp
+++ 5_make.cpp
@@ -1,19 +1,31 @@
-
-
-/*
-
-Write a program whose inputs are three integers, and whose output is the smallest of the three values.
-
-Ex: If the input is:
-
-7 15 3
-the output is:
-
-*/
-
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <string>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //srand(123); // Seed the random number generator
+
+    // Flip a coin 500 times
+    std::vector <std::string> coinFlips;
+    string coinFlipResult = "";
+    
+    // write a for loop that iternates over 500 flips and counts heads or tails in the coinFlips vector
+
+
+    // Count the number of heads
+    int headsCount = 0;
+
+    //handy code to iterate through a vector.  I'll give this to you for future reference.
+    //the auto& is a nice way to have c++ declare the variable as whatever type the variable that follows is first assigned to!  In this case it will be a String, because conflips is declared that way earlier
+    for (const auto& flip : coinFlips) {  
+        if (flip == "Heads") {
+            ++headsCount;
+        }
+    }
+    std::cout << "Number of Heads: " <<  headsCount << '\n';
+    std::cout << "Number of Tails: " << coinFlips.size() - headsCount << '\n';
+
     return 0;
 }
